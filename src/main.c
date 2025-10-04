43a963d60ca6d405a93eae6e18a7fdbd9d18ccba
Linus <linus@libewa.xyz>
2025-10-04 13:44:29 +0200

    add a cursor that can be moved around

--- src/main.c
+++ src/main.c
@@ -1,20 +1,32 @@
 #include <graphx.h>
-#include <ti/getcsc.h>
+#include <ti/getkey.h>
+#include <keypadc.h>
+#include <sys/timers.h>
 
 #include "gfx/gfx.h"
 
 #define SCALE 3
 
+const int tile_width = sprite_tile_width * SCALE;
+const int tile_height = sprite_tile_height * SCALE;
+
+int cursorX = 4;
+int cursorY = 4;
+
 void drawGrid(void) {
     for (int x = 0; x < 9; x++) {
-        for (int y = 0; y < 9; y++) {
-            gfx_ScaledSprite_NoClip(sprite_tile, sprite_tile_width * SCALE * x, sprite_tile_height * SCALE * y, SCALE, SCALE);
+        for (int y = 0; y < 9   ; y++) {
+            gfx_ScaledSprite_NoClip(sprite_tile, tile_width * x, tile_height * y, SCALE, SCALE);
         }
     }
 }
 
 void drawSpriteInGrid(const uint8_t* sprite, int gridX, int gridY) {
-    gfx_ScaledSprite_NoClip(sprite, sprite_tile_width * SCALE * gridX + 2 * SCALE, sprite_tile_height * SCALE * gridY + 2 * SCALE, SCALE, SCALE);
+    gfx_ScaledSprite_NoClip(sprite, tile_width * gridX + 2 * SCALE, tile_height * gridY + 2 * SCALE, SCALE, SCALE);
+}
+
+void drawCursorInGrid(int gridX, int gridY) {
+    gfx_TransparentSprite_NoClip(sprite_cursor, tile_width * gridX + tile_width / 2, tile_height * gridY + tile_height / 2);
 }
 
 void generateMines(void) {
@@ -22,26 +34,51 @@ void generateMines(void) {
 }
 
 int main(void) {
+    kb_EnableOnLatch();
+    kb_ClearOnLatch();
     gfx_Begin();
     gfx_SetPalette(global_palette, sizeof_global_palette, 0);
     
     gfx_SetTransparentColor(0);
     gfx_FillScreen(0);
 
-    drawGrid();
+    while (!kb_On) {
+        kb_Scan();
+        int key = kb_Data[7];
 
-    drawSpriteInGrid(sprite_mine, 0, 0);
-    drawSpriteInGrid(sprite_flag, 1, 1);
-    drawSpriteInGrid(sprite_1, 2, 2);
-    drawSpriteInGrid(sprite_2, 3, 3);
-    drawSpriteInGrid(sprite_3, 4, 4);
-    drawSpriteInGrid(sprite_4, 5, 5);
-    drawSpriteInGrid(sprite_5, 6, 6);
-    drawSpriteInGrid(sprite_6, 7, 7);
-    drawSpriteInGrid(sprite_7, 8, 8);
-    drawSpriteInGrid(sprite_8, 9, 9);
+        if (key & kb_Left && cursorX > 0) {
+            cursorX--;
+        }
+        if (key & kb_Right && cursorX < 8) {
+            cursorX++;
+        }
+        if (key & kb_Up && cursorY > 0) {
+            cursorY--;
+        }
+        if (key & kb_Down && cursorY < 8) {
+            cursorY++;
+        }
 
-    while (!os_GetCSC());
+        delay(70);
+
+        drawGrid();
+        drawSpriteInGrid(sprite_mine, 0, 0);
+        drawSpriteInGrid(sprite_flag, 1, 1);
+        drawSpriteInGrid(sprite_1, 2, 2);
+        drawSpriteInGrid(sprite_2, 3, 3);
+        drawSpriteInGrid(sprite_3, 4, 4);
+        drawSpriteInGrid(sprite_4, 5, 5);
+        drawSpriteInGrid(sprite_5, 6, 6);
+        drawSpriteInGrid(sprite_6, 7, 7);
+        drawSpriteInGrid(sprite_7, 8, 8);
+        drawSpriteInGrid(sprite_8, 0, 1);
+        drawCursorInGrid(cursorX, cursorY);
+
+        gfx_SwapDraw();
+    }
+
     gfx_End();
-    return 0;
+    kb_ClearOnLatch();
+
+    kb_DisableOnLatch();
 }
